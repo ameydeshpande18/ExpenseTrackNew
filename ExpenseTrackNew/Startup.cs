74f0e12ade5443fb0bc0f51f853743d90149fba9
Amey <Amey@DESKTOP-AMEY>
2017-07-03 23:02:57 +0530

    Add project files.

---
+++ ExpenseTrackNew/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExpenseTrackNew.Startup))]
+namespace ExpenseTrackNew
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
